3f213f7e8665701a3ca7417024742f3f95b9c276
araratmaster <ogannisyanaa@digiterus.com>
2024-03-28 11:28:52 +0300

    Add project files.

---
+++ VoprosOtvet/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VoprosOtvet.Models;
+
+namespace VoprosOtvet.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<VoprosOtvet.Models.Joke>? Joke { get; set; }
+    }
+}
